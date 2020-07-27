aee8d355765fc1e61515731c038eb78719b72936
Aswin Gokulachandran <aswin@saavn.com>
2020-07-27 12:52:05 +0530

    - Removing the src/resources/jsonschema - Adding Album Get details, features and pojo classes - Trying fix my search pojo classes which lost all the lombok annotations

---
+++ src/test/java/stepDefinitions/AlbumGetDetails.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package stepDefinitions;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import endPoints.APIResources;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import resources.ConfigReader;
+import resources.Util;
+import statusCodes.StatusCode;
+
+/**
+ * @author aswingokulachandran
+ *
+ */
+public class AlbumGetDetails extends Util {
+
+	RequestSpecification reqSpec;
+	ResponseSpecification resSpec;
+	Response resp;
+
+	@Given("Payload with endPoint {string}")
+	public void payload_with_end_point(String endpoint) throws IOException {
+		APIResources resourceAPI = APIResources.valueOf(endpoint);
+		String resource = resourceAPI.getResource();
+		System.out.println("resource: " + resource);
+		reqSpec = given().spec(requestSpecification(ConfigReader.getInstance().getCtx(), resource));
+	}
+
+	@When("User calls https request with {string}")
+	public void user_calls_https_request_with(String albumid) {
+		resSpec = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectContentType(ContentType.fromContentType("text/html;charset=UTF-8")).build();
+		reqSpec.queryParam("albumid", albumid);
+		System.out.println("resSpec: " + resSpec.toString());
+		resp = reqSpec.given().log().all().when().get("/api.php").then().log().all().extract().response();
+		System.out.println("Aswin response: " + resp.asString());
+
+		logResponseTime(resp);
+
+	}
+
+	@Then("User should get response with status code {string}")
+	public void user_should_get_response_with_status_code(String statusCode) {
+		StatusCode code = StatusCode.valueOf(statusCode);
+		int resource = code.getResource();
+		assertEquals(resp.getStatusCode(), resource);
+	}
+
+	@Then("User should see the response validated")
+	public void user_should_see_the_response_validated() {
+		System.out.println("TODO: JSON Validation");
+	}
+
+}
