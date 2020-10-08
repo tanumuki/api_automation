7b81fe09df127517acdab747e7b58e1d329bac3b
Aswin Gokulachandran <aswin@saavn.com>
2020-10-08 17:01:23 +0530

    Removing featured radio run in runner

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (
-		features = "src/test/java/features/FeaturedRadio.feature",
+		features = "src/test/java/features",
 		glue = {"stepDefinitions"},
 		plugin = {"pretty", "html:target/aswinCukeReport.html", "rerun:target/rerun.txt", "json:target/jsonReport.json","timeline:target/timelineReport"}
 
