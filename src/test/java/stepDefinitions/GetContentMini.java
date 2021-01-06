6b2ba8e2b3254cf3978ea492fbe04af74f7264ae
ashwinsriv <ashwin@saavn.com>
2021-01-06 15:45:44 +0530

    Minor change in handling two different response types

--- src/test/java/stepDefinitions/GetContentMini.java
+++ src/test/java/stepDefinitions/GetContentMini.java
@@ -61,18 +61,36 @@ public class GetContentMini extends Util {
         assertEquals(resp.getStatusCode(), resource);
     }
 
-    @Then("User should successfully validate the Get All contents response")
-    public void user_should_successfully_validate_the_Get_All_contents_response() throws JsonProcessingException {
+    @Then("User should successfully validate the Get All contents response of type {string}")
+    public void user_should_successfully_validate_the_Get_All_contents_response(String entityType) throws JsonProcessingException {
+//      Here we're using "type" because the response type of charts is different from playlists. Charts response has status and data fields,
+//        whereas playlist response has an array of playlist mini objects
+
         ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-        PlaylistContainer contentMinis = mapper.readValue(resp.asString(), PlaylistContainer.class);
         SoftAssert sa = new SoftAssert();
 
+        if(entityType.equalsIgnoreCase("chart")) {
+            PlaylistContainer contentMinis = mapper.readValue(resp.asString(), PlaylistContainer.class);
 //        Validators
-        sa.assertTrue(contentMinis.getStatus().equalsIgnoreCase(Validate.API_STATUS_SUCCESS),
-                "Expected \"" + Validate.API_STATUS_SUCCESS + "\", but found: \"" + contentMinis.getStatus() + "\"");
-        for(PlaylistMini ch : contentMinis.getData()) {
-            new PlaylistMiniValidator().validate(ch, sa);
+            sa.assertTrue(contentMinis.getStatus().equalsIgnoreCase(Validate.API_STATUS_SUCCESS),
+                    "Expected \"" + Validate.API_STATUS_SUCCESS + "\", but found: \"" + contentMinis.getStatus() + "\"");
+            for(PlaylistMini ch : contentMinis.getData()) {
+                new PlaylistMiniValidator().validate(ch, sa);
+            }
         }
+
+        else if(entityType.equalsIgnoreCase("playlist")) {
+            List<PlaylistMini> contentMinis = mapper.readValue(resp.asString(), new TypeReference<List<PlaylistMini>>() {});
+            for(PlaylistMini ch : contentMinis) {
+                new PlaylistMiniValidator().validate(ch, sa);
+            }
+        }
+
+        else {
+//            purposefully fail the assertions for unhandled types. Specific types must be provided from the feature file
+            sa.assertTrue(0>1);
+        }
+
         sa.assertAll();
     }
 
