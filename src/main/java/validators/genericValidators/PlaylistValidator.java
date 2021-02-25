9c36b4e414ee464f0db30d867f3a58b42ae4e1ac
Aswin Gokulachandran <aswin@saavn.com>
2021-02-25 15:49:32 +0530

    PR review changes - Web release related changes 25-Feb-2021

--- src/main/java/validators/genericValidators/PlaylistValidator.java
+++ src/main/java/validators/genericValidators/PlaylistValidator.java
@@ -86,6 +86,8 @@ public class PlaylistValidator extends EntityValidator {
         //Validate subtype
         if(moreInfo.getSubtype() instanceof List){
             sa.assertTrue(((List<?>)moreInfo.getSubtype()).size() == 0, "Playlist more_info.subtype of type list has contents");
+        } else {
+            sa.fail("Unsupported subtype for playlist with id - " + plObj.getId());
         }
         /*
         else {
