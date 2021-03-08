ebda3dcc293dd29b36020fd993ad8b459cdb1b32
Arpit Singh <arpit@saavn.com>
2021-03-08 16:53:32 +0530

    Arpit Singh | added failure condition

--- src/main/java/validators/JioSendOtpValidator.java
+++ src/main/java/validators/JioSendOtpValidator.java
@@ -7,24 +7,21 @@ import pojos.JioSendOtp.JioSendOtpPojo;
 @Slf4j
 public class JioSendOtpValidator {
     String className = getClass().getSimpleName();
-    public void validate(JioSendOtpPojo jioSendOtpPojo, SoftAssert sa)
-    {
+
+    public void validate(JioSendOtpPojo jioSendOtpPojo, SoftAssert sa) {
         String status = jioSendOtpPojo.getStatus();
-        sa.assertTrue(Validate.asStatus(status),className + "." + "validate status failed");
+        sa.assertTrue(Validate.asStatus(status), className + "." + "validate status failed");
         log.info("LOG response status in jio send otp API is " + status);
 
-        if (status.equalsIgnoreCase("success"))
-        {
+        if (status.equalsIgnoreCase("success")) {
             String correlation_id = jioSendOtpPojo.getData().getCorrelation_id();
             sa.assertTrue(Validate.asCorrelationId(correlation_id), className + "." + "validate correlation id failed");
             log.info("LOG response status in jio send otp API is " + correlation_id);
-        }
-        else if (status.equalsIgnoreCase("error"))
-        {
+        } else if (status.equalsIgnoreCase("error")) {
             String msg = jioSendOtpPojo.getMsg();
-            sa.assertEquals(msg, "Your OTP could not be sent because there were too many unverified requests. Please try again later.", className + "." + "validate msg failed");
+            sa.assertTrue(msg.equalsIgnoreCase("Your OTP could not be sent because there were too many unverified requests. Please try again later.") || msg.equalsIgnoreCase("Request is missing required parameters or parameters are not valid"), className + "." + "validate msg failed");
             log.info("LOG response msg in jio send otp API is " + msg);
         }
-
     }
+
 }
--- src/test/java/features/JioSendOtp.feature
+++ src/test/java/features/JioSendOtp.feature
@@ -8,4 +8,12 @@ Feature: Validating Jio Send OTP API
       | +919372804795 |
     Then The Jio Send OTP API returns response with status code 200
 
+  Scenario: Verify if user is not able to generate an OTP using Jio Send OTP API with an incorrect number
+
+    Given I have the endpoint for "JioSendOtpApi"
+    When I make the "GET" request with the following query parameters
+      | phone_number  |
+      | 9372804795 |
+    Then The Jio Send OTP API returns response with status code 200
+
 
