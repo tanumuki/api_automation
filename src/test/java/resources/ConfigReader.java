6a4e5e9bb6e1ed00d5b2e4642df56411ee87c16e
ashwinsriv <ashwin@saavn.com>
2021-05-19 12:20:36 +0530

    configreader brevity update

--- src/test/java/resources/ConfigReader.java
+++ src/test/java/resources/ConfigReader.java
@@ -75,32 +75,11 @@ public class ConfigReader {
 		
 
 			if (app_version != null) {
-				if (app_version.equalsIgnoreCase("5.1")) {
-					properties.setProperty("app_version", "5.1");
-				} else if (app_version.equalsIgnoreCase("6.1")) {
-					properties.setProperty("app_version", "6.1");
-				}
-				else if (app_version.equalsIgnoreCase("7.5.1")) {
-					properties.setProperty("app_version", "7.5.1");
-				}
-				else if (app_version.equalsIgnoreCase("7.4.0")){
-					properties.setProperty("app_version", "7.4.0");
-				}
+				properties.setProperty("app_version", app_version);
 			}
 
 			if (version != null) {
-				if (version.equalsIgnoreCase("190")) {
-					properties.setProperty("v", "190");
-				}
-				else if (version.equalsIgnoreCase("211")) {
-					properties.setProperty("v", "211");
-				}
-				else if (version.equalsIgnoreCase("258")) {
-					properties.setProperty("v", "258");
-				}
-				else if (version.equalsIgnoreCase("740.1")){
-					properties.setProperty("v", "740.1");
-				}
+					properties.setProperty("v", version);
 			}
 
 			if (ctx != null) {
