3ac58985a907fe3f18c61104ac43656851fae7a7
Snigdha Majee <snigdha.majee@saavn.com>
2021-06-07 15:31:19 +0530

    Called method: AssertionMsg.print(String className, String methodName, String fieldName, String value)

--- src/main/java/validators/genericValidators/ArtistDedicatedPlaylistsValidator.java
+++ src/main/java/validators/genericValidators/ArtistDedicatedPlaylistsValidator.java
@@ -3,7 +3,10 @@ package validators.genericValidators;
 import entities.*;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.asserts.SoftAssert;
+import validators.AssertionMsg;
 import validators.PlaylistMiniValidator;
+import validators.Validate;
+
 import java.util.Map;
 
 /**
@@ -16,10 +19,11 @@ public class ArtistDedicatedPlaylistsValidator {
     final String className = getClass().getName();
 
     public void validate(DedicatedArtistPlaylist dedicatedArtistPlaylistPojo, SoftAssert sa, String artistID) {
+        final String methodName = new Throwable().getStackTrace()[0].getMethodName();
         for (Map.Entry<String, PlaylistMini[]> data : dedicatedArtistPlaylistPojo.getArtistID().entrySet()) {
             System.out.println(dedicatedArtistPlaylistPojo.getArtistID().entrySet());
             String key_artistid = data.getKey();
-            sa.assertEquals(key_artistid, artistID, className + ": artistid's don't match");
+            sa.assertEquals(key_artistid, artistID, AssertionMsg.print(className, methodName, "artistID", artistID));
 
             for (PlaylistMini playlistMiniPojo : data.getValue()) {
                 new PlaylistMiniValidator().validate(playlistMiniPojo, sa);
